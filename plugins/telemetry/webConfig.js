6dfa6ecb336432b3b0ad054ce33f4e24f27a3f11
pingguo114514 <1135636981@qq.com>
2025-04-05 23:26:29 +0800

    feat: 移除网页面板

--- plugins/telemetry/webConfig.js
+++ plugins/telemetry/webConfig.js
@@ -7,49 +7,14 @@ class WebConfigTye{
     static buttom = 6;
 }
 
-
 class WebConfigBuilder{
-    plname;
-    configObj = {};
-    constructor(plname){
-        this.plname = plname;
-    }
-    addEditArray(k,v,desc = "描述"){
-       this.#setKV(k,v,WebConfigTye.EditArray,desc);
-    }
-    addChoosing(k,opt,v,desc = "描述"){
-        if(this.configObj[k]){
-            throw new Error(`plugin ${this.plname} add a already key (${k}) when build web config`);
-        }
-        this.configObj[k] = {
-            type: WebConfigTye.Choosing,
-            value: v,
-            options: opt,
-            desc
-        }
-    }
-    addSwitch(k,v,desc = "描述"){
-        this.#setKV(k,v,WebConfigTye.switch,desc);
-    }
-    addText(k,v,desc = "描述"){
-        this.#setKV(k,v,WebConfigTye.text,desc);
-    }
-    addNumber(k,v,desc = "描述"){
-        this.#setKV(k,v,WebConfigTye.number,desc);
-    }
-    addButtom(k,v,desc = "描述"){
-        this.#setKV(k, v, WebConfigTye.buttom, desc);
-    }
-    #setKV(k,v,t,desc){
-        if(this.configObj[k]){
-            throw new Error(`plugin ${this.plname} add a already key (${k}) when build web config`);
-        }
-        this.configObj[k] = {
-            type: t,
-            value: v,
-            desc
-        }
-    }
+    constructor(){}
+    addEditArray(){}
+    addChoosing(){}
+    addSwitch(){}
+    addText(){}
+    addNumber(){}
+    addButtom(){}
 }
 
 module.exports = {WebConfigTye,WebConfigBuilder};
